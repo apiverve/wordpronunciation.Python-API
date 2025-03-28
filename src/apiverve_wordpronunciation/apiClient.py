f613f539c88719f43a136343256d0c893b333ead
APIVerve Team <hello@apiverve.com>
2025-03-28 18:07:53 -0500

    Bug Fixes

--- src/apiverve_wordpronunciation/apiClient.py
+++ src/apiverve_wordpronunciation/apiClient.py
@@ -10,10 +10,10 @@ class WordpronunciationAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/wordpronunciation'
+        self.base_url = 'https://api.apiverve.com/v1/wordpronunciation'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
